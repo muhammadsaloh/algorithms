190ab447a56ea5a2a866abce53c89eca79c2a09b
salohiddin17 <salohiddindilmatov9@gmail.com>
2021-09-02 12:16:27 +0500

    old

---
+++ payme1.js
@@ -0,0 +1,17 @@
+// function customReverse(array) {
+//     let arr = [];
+//     for(let i = array.length - 1; i >= 0; i--){
+//         arr.push(array[i])
+//     }
+//     return arr;
+// }
+
+// console.log(
+//     customReverse(
+//         [1, 2, 3, 4, 5]
+//     )
+// )
+
+function palindrome (array, num) {
+
+}
